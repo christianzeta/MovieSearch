620a00a2ca7ff8511ddec7bd38d810d670173f56
Christian Zetterholm <christian.zetterholm@utb.ecutbildning.se>
2021-09-02 10:02:03 +0200

    Add project files.

---
+++ MovieSearch/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using MovieSearch.Models;
+using System.Net.Http.Json;
+
+namespace MovieSearch.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory clientFactory)
+        {
+            _logger = logger;
+        }
+
+
+        public void OnGet()
+        {
+            
+        }
+
+    }
+}
---
+++ MovieSearch/Pages/MovieInfo.cshtml.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MovieSearch.Models;
+
+namespace MovieSearch.Pages
+{
+    public class MovieInfoModel : PageModel
+    {
+        private readonly ILogger<MovieInfoModel> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public MovieModel Movie { get; set; } = null;
+
+        [BindProperty]
+        public string Title { get; set; }
+
+        public MovieInfoModel(ILogger<MovieInfoModel> logger, IHttpClientFactory clientFactory)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+        }
+
+        private static async Task<MovieModel> MakeRequest(IHttpClientFactory factory, string title)
+        {
+            var query = $"http://www.omdbapi.com/?apikey=78e51e6c&t={title}";
+            var request = new HttpRequestMessage(HttpMethod.Get, query);
+            var client = factory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                MovieModel movie = await response.Content.ReadFromJsonAsync<MovieModel>();
+                return movie;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task OnPostAsync()
+        {
+            Movie = await MakeRequest(_clientFactory, Title);
+        }
+    }
+}
